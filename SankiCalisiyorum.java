ec955d780e06ea9336ccd80f354b951d3ee784ba
Scarlett <e.birgul@gmail.com>
2022-10-04 13:32:12 -0500

    Conflict

--- SankiCalisiyorum.java
+++ SankiCalisiyorum.java
@@ -1,6 +1,6 @@
 public class SankiCalisiyorum {
     public static void main(String[] args) {
         System.out.println("merhaba ben calismaya calisiyorum");
-
+        System.out.println("helloWorld1");
     }
 }
